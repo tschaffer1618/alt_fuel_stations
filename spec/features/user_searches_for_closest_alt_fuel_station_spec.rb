46e4b6a7eebffee534eabca65b3e15e2d71d363c
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-25 11:26:03 -0600

    Add sections to search page

--- spec/features/user_searches_for_closest_alt_fuel_station_spec.rb
+++ spec/features/user_searches_for_closest_alt_fuel_station_spec.rb
@@ -10,13 +10,18 @@ describe 'A user' do
 
     expect(current_path).to eq search_path
 
-    expect(page).to have_content("Seventeenth Street Plaza")
-    expect(page).to have_content("1225 17th St., Denver, CO 80202")
-    expect(page).to have_content("Hours of Service: MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified")
-    expect(page).to have_content("Distance: 0.0934 mi")
-    expect(page).to have_content("Fuel Type: ELEC")
-    expect(page).to have_content("Distance: 0.1 mi")
-    expect(page).to have_content("Time: 1 min")
-    expect(page).to have_content("Steps: Head <b>southeast</b> on <b>17th St</b> toward <b>Larimer St</b>")
+    within('.station-info') do
+      expect(page).to have_content("Seventeenth Street Plaza")
+      expect(page).to have_content("1225 17th St., Denver, CO 80202")
+      expect(page).to have_content("Hours of Service: MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified")
+      expect(page).to have_content("Distance: 0.0934 mi")
+      expect(page).to have_content("Fuel Type: ELEC")
+    end
+
+    within('.route-info') do
+      expect(page).to have_content("Distance: 0.1 mi")
+      expect(page).to have_content("Time: 1 min")
+      expect(page).to have_content("Steps: Head <b>southeast</b> on <b>17th St</b> toward <b>Larimer St</b>")
+    end
   end
 end
