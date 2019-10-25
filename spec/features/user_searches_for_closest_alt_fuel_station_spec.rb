1d9d012c9211d299fc60f4563ba8ffacc91c0df5
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-25 09:18:53 -0600

    Create search controller and view for search index

--- spec/features/user_searches_for_closest_alt_fuel_station_spec.rb
+++ spec/features/user_searches_for_closest_alt_fuel_station_spec.rb
@@ -1,23 +1,37 @@
-The NREL alternate fuel stations nearest station API: https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/nearest/
-The Google Directions API: https://developers.google.com/maps/documentation/directions/start
-We will be searching for the nearest electric charging station to Turing.
+# The NREL alternate fuel stations nearest station API: https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/nearest/
+# The Google Directions API: https://developers.google.com/maps/documentation/directions/start
+# We will be searching for the nearest electric charging station to Turing.
+#
+# As a user
+# When I visit "/"
+# And I select "Turing" form the start location drop down (Note: Use the existing search form)
+# And I click "Find Nearest Station"
+# Then I should be on page "/search"
+# Then I should see the closest electric fuel station to me.
+#
+# For that station I should see
+# - Name
+# - Address
+# - Fuel Type
+# - Distance
+# - Access Times
+#
+# I should also see:
+# - the distance of the nearest station
+# - the travel time from Turing to that fuel station (should be 0.1 miles)
+# - The HTML direction instructions to get to that fuel station
+#   "Head <b>southeast</b> on <b>17th St</b> toward <b>Larimer St</b>"
 
-As a user
-When I visit "/"
-And I select "Turing" form the start location drop down (Note: Use the existing search form)
-And I click "Find Nearest Station"
-Then I should be on page "/search"
-Then I should see the closest electric fuel station to me.
+require 'rails_helper'
 
-For that station I should see
-- Name
-- Address
-- Fuel Type
-- Distance
-- Access Times
+describe 'A user' do
+  scenario 'can find the nearest station with pertinent info' do
+    visit '/'
 
-I should also see:
-- the distance of the nearest station
-- the travel time from Turing to that fuel station (should be 0.1 miles)
-- The HTML direction instructions to get to that fuel station
-  "Head <b>southeast</b> on <b>17th St</b> toward <b>Larimer St</b>"
+    select 'Turing', from: 'location'
+
+    click_button 'Find Nearest Station'
+
+    expect(current_path).to eq search_path
+  end
+end
